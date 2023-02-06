4a6b9412bff005470ded2898e6bc207f54798c3b
ochosteve <ochosteve08@gmail.com>
2023-02-06 14:42:10 +0100

    Updates

---
+++ precache-manifest.6f5edb3025d810fd6243c1c595849764.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "36e5a3038e0b6d84ee72",
+    "url": "/Weather-App/static/css/main.438d5939.chunk.css"
+  },
+  {
+    "revision": "36e5a3038e0b6d84ee72",
+    "url": "/Weather-App/static/js/main.36e5a303.chunk.js"
+  },
+  {
+    "revision": "f448b409c2671c952587",
+    "url": "/Weather-App/static/js/1.f448b409.chunk.js"
+  },
+  {
+    "revision": "edcfa8a852f959e2feef",
+    "url": "/Weather-App/static/js/runtime~main.edcfa8a8.js"
+  },
+  {
+    "revision": "f408a0db2919f7bceac0f47568acc503",
+    "url": "/Weather-App/index.html"
+  }
+];
