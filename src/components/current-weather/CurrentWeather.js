64f45f7e8c351100c24390ab5fd8bbc8ef0900c5
ochosteve <ochosteve08@gmail.com>
2023-02-07 04:14:50 +0100

    fixed temperature

--- src/components/current-weather/CurrentWeather.js
+++ src/components/current-weather/CurrentWeather.js
@@ -14,14 +14,14 @@ const CurrentWeather = ({data}) =>{
             <img src={`icons/${data.weather[0].icon}.png`} alt="weather" className="weather-icon" />
             </div>
             <div className="bottom">
-                <p className="temperature">{data.main.temp}°C</p>
+                <p className="temperature">{Math.round(data.main.temp)}°C</p>
                 <div className="details">
                     <div className="parameter-row">
                         <span className="parameter-label">Details</span>
                     </div>
                     <div className="parameter-row">
                         <span className="parameter-label">Feels like</span>
-                        <span className="parameter-value">{data.main.feels_like}°C</span>
+                        <span className="parameter-value">{Math.round(data.main.feels_like)}°C</span>
                     </div>
                     <div className="parameter-row">
                         <span className="parameter-label">Wind</span>
